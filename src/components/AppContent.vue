ded23c64f198030cea056036d8bf8045730dedf4
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-29 15:09:56 +0100

    Add: AppContent.vue WIP

---
+++ src/components/AppContent.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<template>
+    <div class="col" v-for="movie in store.movies" key:="movie.id">
+
+    
+        <div class="card">
+            <p> Titolo: {{ movie.title }}</p>
+            <p> Titolo originale: {{ movie.original_title  }}</p>
+            <p> Lingua: {{ movie.original_language  }}</p>
+            <p> Voto: {{ movie.vote_average }} </p>
+        </div>
+    </div>
+</template>
+
+<script scoped>
+</script>
